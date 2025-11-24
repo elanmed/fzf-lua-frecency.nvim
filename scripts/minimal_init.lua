4c37d8d967a51f718af5d9556b72f16c5185c05b
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-11-24 01:16:31 -0500

    tests: update minimal_init to include fzf-lua as a dep

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -3,5 +3,6 @@ vim.cmd [[let &rtp.=','.getcwd()]]
 
 if #vim.api.nvim_list_uis() == 0 then
   vim.cmd "set rtp+=deps/mini.nvim"
+  vim.cmd "set rtp+=deps/fzf-lua"
   require "mini.test".setup()
 end
